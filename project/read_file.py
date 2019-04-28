9c8699c351e7140d6d93c19f7e560ffe0e94a413
YiLiM1 <547384994@qq.com>
2019-04-28 19:48:51 +0800

    源程序

--- project/read_file.py
+++ project/read_file.py
@@ -53,8 +53,9 @@ def read_protein(Concept_G,Physical_G):
             if not lines:
                 break
             list1=lines.split("\n")
-            dict_C[str(i)] = list1[0]
+            dict_P[str(i)] = list1[0]
             list_P.append(list1[0])
+            #print(str(i), ":", list1[0])
             i+=1
     file_to_read.close()
     with open(os.path.join(path1, "数据集\\Protein_Concept_Nodes.txt"), 'r') as file_to_read:
@@ -63,27 +64,32 @@ def read_protein(Concept_G,Physical_G):
             lines = file_to_read.readline() # 整行读取数据
             if not lines:
                 break
-            list1=lines.split("\n")
+            list1=lines.split(" ")
             dict_C[str(i)] =list1[0]
             list_C.append(list1[0])
+            #print(str(i),":",list1[0])
             i += 1
     file_to_read.close()
 
+
     list_same=[n for n in list_C if n in list_P]  #双网络的公共节点
+    #print(len(list_same))
 
     C_G = nx.Graph()
     P_G = nx.Graph()
-
+    print(dict_C)
+    print(dict_P)
     with open(os.path.join(path1, "数据集\\Protein_Physical_Edges.txt"), 'r') as file_to_read:
         while True:
             lines = file_to_read.readline() # 整行读取数据
             if not lines:
               break
             list1 = lines.split(" ")
+            list1[1]=list1[1][0:len(list1[1])-1]
             print(list1[0], list1[1])
             print(dict_P[list1[0]],dict_P[list1[1]])
             if dict_P[list1[0]] in list_same and dict_P[list1[1]] in list_same:
-                Physical_G.add_edge(dict_C[list1[0]], dict_C[list1[1]])
+                Physical_G.add_edge(dict_P[list1[0]], dict_P[list1[1]])
     file_to_read.close()
 
     with open(os.path.join(path1, "数据集\\Protein_Concept_Edges.txt"), 'r') as file_to_read:
@@ -92,7 +98,6 @@ def read_protein(Concept_G,Physical_G):
             if not lines:
               break
             list1 = lines.split(" ")
-            print(list1[0], list1[1])
             if dict_C[list1[0]] in list_same and dict_C[list1[1]] in list_same:
                Concept_G.add_edge(dict_C[list1[0]], dict_C[list1[1]])
     file_to_read.close()
